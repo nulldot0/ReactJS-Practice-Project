ca2dc72d9215b8625550b5b0890005aec9fa3255
jong <jhpetalbo@gmail.com>
2020-11-05 23:59:35 +0800

    initial commit

---
+++ react101/src/components/header.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+
+class Header extends react.Component {
+    render() {
+        return (
+            <header>
+                <h1>TODOLIST APP</h1>
+            </header>
+        )
+    }
+}
+
+export default Header
